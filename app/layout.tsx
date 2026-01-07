8f7fa30b6c5e1edef3743b1f2a8a8fc14b1e5077
mdanassaif <oliwertwist9@gmail.com>
2026-01-08 02:49:11 +0530

    check who is using it

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: "SnoringPunch 👊 - Virtual Roommate Therapy",
@@ -25,6 +26,7 @@ export default function RootLayout({
     <html lang="en">
       <body className="antialiased">
         {children}
+        <Analytics />
       </body>
     </html>
   );
